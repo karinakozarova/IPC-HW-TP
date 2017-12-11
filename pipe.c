69eb3a31a7f14e6fabc266d99003cc2f0a22fb58
karinakozarova <karinakozarova1303@gmail.com>
2017-12-11 14:21:16 +0200

    mroe than one read support

--- pipe.c
+++ pipe.c
@@ -1,31 +1,29 @@
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <stdio.h>
+#include <fcntl.h>              
 #include <unistd.h>
-#include <stdio.h>
 #include <string.h>
-#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
-
-int main(){
+int main()
+{   
     char msg[1024],ch;
     int pipefd[2];
     pipe(pipefd);
-
     printf("ENTER MESSAGE:\n");
-    fgets(msg, sizeof(msg), stdin);
-    printf("YOUR MESSAGE:\n");
+
     pid_t pid = fork();
 
-    if(pid == 0){
+    if (pid == 0){
         close(pipefd[1]);
-        while(read(pipefd[0],&ch,1) > 0){
-            write(STDOUT_FILENO,&ch,1);
-        }
-    }else{
-        close(pipefd[0]);
-        write(pipefd[1],msg,strlen(msg));
+        for(;;) while(read(pipefd[0], &ch, 1) > 0) write(STDOUT_FILENO, &ch, 1);
     }
-
-    return 0;
+    else{
+        for(;;){
+            close(pipefd[0]);
+            fgets(msg, sizeof(msg), stdin);
+            printf("YOUR MESSAGE:\n");
+            write(pipefd[1], msg, strlen(msg));
+        }   
+    }
 }
